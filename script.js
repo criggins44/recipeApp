15192716813a352137fd02dbd6037e82aa71133a
criggins44 <128842735+criggins44@users.noreply.github.com>
2024-07-16 16:50:09 -0400

    added code to load the recipe when the page loads

--- script.js
+++ script.js
@@ -9,35 +9,8 @@ let videoBtn = document.getElementById("video-btn");
 //thumbNail.innerHTML = `insert thumbnail here`;
 //displayResults.innerHTML = `display recipe results here`;
 
-/*
-axios.get(url).then((res)=> {
-    let myMeal = res.data.meals[0];
-    let ingredientList = [];
-    let count = 1;
-    for(i in myMeal){
-        let ingredient = "";
-        let measure = "";
-        if (i.startsWith("strIngredient") && myMeal[i]) {
-            ingredient = myMeal[i];
-            measure = myMeal[`strMeasure` + count];
-            count += 1;
-            console.log(ingredient, measure);
-            ingredientList.push(`${measure} ${ingredient}`);
-        }
-    };
-    console.log(ingredientList);
-    let list = document.createElement("ul");
-
-    ingredientList.forEach((i) => {
-        let listItem = document.createElement("li");
-        listItem.innerText = i;
-        list.appendChild(listItem);
-        displayContainer.appendChild(list);
-    })
-});
-*/
-
-recipeBtn.addEventListener("click", function(){
+//Code below duplicates the "get recipe" function, but displays the recipe when the page is loaded
+document.addEventListener("DOMContentLoaded", function(){
     axios.get(url).then(res => {
         let myMeal = res.data.meals[0];
         thumbNail.innerHTML = `
@@ -97,51 +70,49 @@ recipeBtn.addEventListener("click", function(){
     }) 
 })
 
-
-
-/*
-axios.get(url).then(res => {
-    let myMeal = res.data.meals[0];
-    thumbNail.innerHTML = `
-    <img id='thumbnail' src=${myMeal.strMealThumb}>
-    <p class='label'>${myMeal.strMeal}</p>
-    <p class='label'>${myMeal.strArea}</p>
-    `
-    displayResults.innerHTML = `
-    ${myMeal.strInstructions}
-    `;
-
-    videoBtn.addEventListener("click", function(){
-        window.open(myMeal.strYoutube)
-    })
-})
-*/
-/*
-recipeBtn.addEventListener("click", () => {
-    axios.get(url)
-    .then(res => {
+recipeBtn.addEventListener("click", function(){
+    axios.get(url).then(res => {
         let myMeal = res.data.meals[0];
-        console.log(myMeal);
-        console.log(myMeal.strMealThumb);
-        console.log(myMeal.strMeal);
-        console.log(myMeal.strArea);
-        console.log(myMeal.strInstructions);
-        let ingredientsList = [];
-        let count = 1;
+        thumbNail.innerHTML = `
+        <img id='thumbnail' src=${myMeal.strMealThumb}>
+        <p class='label'>${myMeal.strMeal}</p>
+        <p class='label'>${myMeal.strArea}</p>
+        `
+        displayContainer.innerHTML = `
+        ${myMeal.strInstructions}
+        `;
 
+        let ingredientList = [];
+        let count = 1;
         for(i in myMeal){
             let ingredient = "";
-            let measurements = "";
-
-            if (i.startsWith("strIngredient") && myMeal[i]){
+            let measure = "";
+            if (i.startsWith("strIngredient") && myMeal[i]) {
                 ingredient = myMeal[i];
-                measurements = myMeal[`strMeasure` + count];
-                count +=1;
-                ingredientsList.push(`${measurements} ${ingredient}`);
+                measure = myMeal[`strMeasure` + count];
+                count += 1;
+                console.log(ingredient, measure);
+                ingredientList.push(`${measure} ${ingredient}`);
             }
+        };
+
+        let list = document.createElement("ul");
+
+        ingredientList.forEach((i) => {
+            let listItem = document.createElement("li");
+            listItem.innerText = i;
+            list.appendChild(listItem);
+            displayContainer.appendChild(list);
+        })
+        
+        let videoClip = () => {
+            window.open(res.data.meals[0].strYoutube);
+            videoBtn.removeEventListener("click", videoClip);
         }
-    })
-    displayResults.innerHTML = `test this`;
-    .catch(err => console.log(err));
+
+        videoBtn.addEventListener("click", videoClip)
+
+//generates new recipe and tutorial video with each mouse click
+
+    }) 
 })
-*/
