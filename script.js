6b6e051702940a946ac4e889624c145f095797fc
criggins44 <128842735+criggins44@users.noreply.github.com>
2024-07-17 19:45:05 -0400

    added getRecipe function to be called by event listeners to generate recipe

--- script.js
+++ script.js
@@ -6,11 +6,12 @@ let thumbNail = document.getElementById("thumbnail-container");
 let recipeBtn = document.getElementById("recipe-btn");
 let videoBtn = document.getElementById("video-btn");
 
+
 //thumbNail.innerHTML = `insert thumbnail here`;
 //displayResults.innerHTML = `display recipe results here`;
 
-//Code below duplicates the "get recipe" function, but displays the recipe when the page is loaded
-document.addEventListener("DOMContentLoaded", function(){
+//creates the function called by eventListeners that populates the recipe and instructions
+let getRecipe = () => {
     axios.get(url).then(res => {
         let myMeal = res.data.meals[0];
         thumbNail.innerHTML = `
@@ -21,8 +22,8 @@ document.addEventListener("DOMContentLoaded", function(){
         displayContainer.innerHTML = `
         ${myMeal.strInstructions}
         `;
-//populates the instructions for making the dish
 
+//populates the instructions for making the dish
         let ingredientList = [];
         let count = 1;
         for(i in myMeal){
@@ -38,7 +39,6 @@ document.addEventListener("DOMContentLoaded", function(){
         };
 
  //creates a bulleted list based on the ingredients from the website       
-
         let list = document.createElement("ul");
 
         ingredientList.forEach((i) => {
@@ -56,63 +56,9 @@ document.addEventListener("DOMContentLoaded", function(){
 
         videoBtn.addEventListener("click", videoClip)
 
-//generates new recipe and tutorial video with each mouse click
-
-        /*
-        videoBtn.addEventListener("click", function(){
-            window.open(res.data.meals[0].strYoutube);
-        })
-
-        videoBtn.removeEventListener("click", function(){
-            window.open(res.data.meals[0].strYoutube);
-        })
-        */
     }) 
-})
+};
 
-recipeBtn.addEventListener("click", function(){
-    axios.get(url).then(res => {
-        let myMeal = res.data.meals[0];
-        thumbNail.innerHTML = `
-        <img id='thumbnail' src=${myMeal.strMealThumb}>
-        <p class='label'>${myMeal.strMeal}</p>
-        <p class='label'>${myMeal.strArea}</p>
-        `
-        displayContainer.innerHTML = `
-        ${myMeal.strInstructions}
-        `;
+document.addEventListener("DOMContentLoaded", getRecipe);
 
-        let ingredientList = [];
-        let count = 1;
-        for(i in myMeal){
-            let ingredient = "";
-            let measure = "";
-            if (i.startsWith("strIngredient") && myMeal[i]) {
-                ingredient = myMeal[i];
-                measure = myMeal[`strMeasure` + count];
-                count += 1;
-                console.log(ingredient, measure);
-                ingredientList.push(`${measure} ${ingredient}`);
-            }
-        };
-
-        let list = document.createElement("ul");
-
-        ingredientList.forEach((i) => {
-            let listItem = document.createElement("li");
-            listItem.innerText = i;
-            list.appendChild(listItem);
-            displayContainer.appendChild(list);
-        })
-        
-        let videoClip = () => {
-            window.open(res.data.meals[0].strYoutube);
-            videoBtn.removeEventListener("click", videoClip);
-        }
-
-        videoBtn.addEventListener("click", videoClip)
-
-//generates new recipe and tutorial video with each mouse click
-
-    }) 
-})
+recipeBtn.addEventListener("click", getRecipe);
